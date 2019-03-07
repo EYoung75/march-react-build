305396950e391afea6511be9ef1379192189de84
EYoung75 <eyoung75.ey@gmail.com>
2019-03-07 12:38:20 -0700

    created and implemented ScrollToTop component to use with router

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from "react-router-dom";  
+import ScrollToTop from "./components/ScrollToTop.jsx";
 
 ReactDOM.render(
 <Router>
-    <App />
+    <ScrollToTop>
+        <App />
+    </ScrollToTop>
 </Router>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
